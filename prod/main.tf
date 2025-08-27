620580169cca11d93fc2820bee553a8eedf14e1a
Mohamed Habib <moe.habib9@gmail.com>
2025-08-27 19:40:36 +0100

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -1,1 +1,10 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.2" # pin to a specific version or range
+    }
+  }
+}
+
 resource "null_resource" "test2" {}
